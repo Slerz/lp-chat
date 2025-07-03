7899717fdb894c1a59f7fcf62476667730089a87
Amir <idrisovamir21@gmail.com>
2025-07-03 18:19:32 +0300

    Update description

--- js/messengerOptions.js
+++ js/messengerOptions.js
@@ -1,7 +1,6 @@
 const messengerOptions = [
   { label: "Telegram", value: "telegram", next: "phone" },
   { label: "WhatsApp", value: "whatsapp", next: "phone" },
-  { label: "Viber", value: "viber", next: "phone" }
 ];
 
 export default messengerOptions; 
