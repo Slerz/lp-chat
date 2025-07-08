69841a085c04bea0e387d1bc7ae6b02eaf646baa
Amir <idrisovamir21@gmail.com>
2025-07-08 16:29:40 +0300

    Update description

--- js/feedback.js
+++ js/feedback.js
@@ -79,7 +79,7 @@ function initFeedbackForm() {
       }
 
       // Отправляем данные на ваш PHP endpoint
-      $.ajax('https://zoological-expression-production.up.railway.app/api/send_contact.php', {
+      $.ajax('https://charismatic-intuition-production.up.railway.app/api/send_contact.php', {
         type: 'POST',
         data: JSON.stringify(formData),
         contentType: 'application/json',
